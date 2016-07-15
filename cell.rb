c7b9a0b3424b86b4da74ae9244cb3aff26576b8a
Grant <ghwilley@gmail.com>
2016-07-15 09:27:52 -0500

    added neighbor finding shiii

--- cell.rb
+++ cell.rb
@@ -1,12 +1,26 @@
 class Cell
-	def initialize(neighbors)
-		@neighbors = neighbors
+	attr_reader :x, :y
+	def initialize(world, x, y)
+		@world = world
 		@alive = false
 	end
 
-	# def check_neighbors
 
-	# end
+	def neighbors
+    neighbors = []
+    neighbors << @world.cell_at(self.x - 1, self.y - 1)
+    neighbors << @world.cell_at(self.x - 1, self.y)
+    neighbors << @world.cell_at(self.x - 1, self.y + 1)
+
+    neighbors << @world.cell_at(self.x, self.y - 1)
+    neighbors << @world.cell_at(self.x, self.y + 1)
+
+    neighbors << @world.cell_at(self.x + 1, self.y - 1)
+    neighbors << @world.cell_at(self.x + 1, self.y)
+    neighbors << @world.cell_at(self.x + 1, self.y + 1)
+
+    neighbors
+  end
 
 	def alive?
 		if @neighbors.length < 2
